73dd192719614b412adec5fa6c4771029bb1e8e3
Jacob Pine <jpinedev@gmail.com>
2024-01-07 17:51:05 -0500

    split up project into multiple executables

---
+++ common/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include <iostream>
+
+void PrintMSG()
+{
+    std::cout << "Hello StaticLib!" << std::endl;
+}
--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,21 @@
 workspace "JobSearchTracker"
     configurations { "Debug", "Release" }
 
+project "TrackerCommon"
+    kind "StaticLib"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    
+    files { "common/**.h", "common/**.cpp" }
+
 project "JobSearchTracker"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
-  
-    files { "**.h", "**.cpp" }
+    
+    files { "tracker/**.h", "tracker/**.cpp" }
+
+    links { "TrackerCommon" }
 
     filter "configurations:Debug"
         defines { "DEBUG" }
@@ -15,4 +24,33 @@ project "JobSearchTracker"
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
+
+    filter { "system:macosx" }
+        includedirs { "/usr/local/include" }
+
+    filter { "system:linux" }
+        includedirs { "/usr/local/include" }
+    
+project "TrackingExporter"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    
+    files { "exporter/**.h", "exporter/**.cpp" }
+
+    links { "TrackerCommon" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+    filter { "system:macosx" }
+        includedirs { "/usr/local/include" }
+
+    filter { "system:linux" }
+        includedirs { "/usr/local/include" }
 
